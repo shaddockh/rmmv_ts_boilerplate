365dc49590b7c6b494e6eb639c817d35829bb643
Shaddock Heath <shaddockh@gmail.com>
2018-03-08 09:55:10 -0600

    set up rtp_overrides folder and added a stub readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# RPG Maker MV TypeScript Boilerplate Project
+
+This is a project template for new RPG Maker MV projects that want to develop scripts in TypeScript.
+
+## Setup
+
+TODO:
--- rmmv.build.config.js
+++ rmmv.build.config.js
@@ -1,6 +1,6 @@
 ///////////////////////////////
 // Configuration file for build
-
+const path = require("path");
 const rmmvBuildConfig = {
     // Path to where the TypeScript plugins are located.  These will be transpiled and
     // bundled to the standard rmmv plugin directory
@@ -10,7 +10,27 @@ const rmmvBuildConfig = {
     // instead of duplicating that code in each bundle.  If this is selected, then that additional bundle
     // will need to be distributed and loaded *first* in the rmmv plugin list
     identifySharedCode: false,
-    sharedCodeBundleName: "commons"
+    sharedCodeBundleName: "commons",
+
+    // This allows for storing assets outside of the RTP and then overriding them on build.  The RTP asset will
+    // either be overwritten by these files or assets will be added to the correct directory
+    copyAdditionalResources: true,
+    copyPatterns: [
+        {
+            from: "**/*.{m4a,ogg}",
+            to: path.join(__dirname, "audio"),
+            context: "rtp_overrides/audio/"
+        },
+        {
+            from: "**/*.{css,ttf}",
+            to: path.join(__dirname, "fonts"),
+            context: "rtp_overrides/fonts/"
+        },
+        { from: "**/*.png", to: path.join(__dirname, "icon"), context: "rtp_overrides/icon/" },
+        { from: "**/*.png", to: path.join(__dirname, "img"), context: "rtp_overrides/img/" },
+        { from: "**/*.*", to: path.join(__dirname, "movies"), context: "rtp_overrides/movies/" },
+        { from: "**/*.png", to: path.join(__dirname, "system"), context: "rtp_overrides/system/" }
+    ]
 };
 
 module.exports = rmmvBuildConfig;
