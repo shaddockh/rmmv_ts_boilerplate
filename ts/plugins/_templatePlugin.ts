bcfe4239643fc381bdc60c027d1ed89681dc06ea
Shaddock Heath <shaddockh@gmail.com>
2018-04-07 19:23:15 -0500

    Update for latest settings

--- ts/plugins/_templatePlugin.ts
+++ ts/plugins/_templatePlugin.ts
@@ -1,1 +1,17 @@
-// Empty Plugin
+/*:
+ * @plugindesc Sample Template Plugin
+ * @author You
+ *
+ * @param My Parameter
+ * @desc My parameter description
+ * @default True
+ *
+ * @help
+ * This is the help section.
+ */
+(function() {
+    const pluginName = "TemplatePlugin";
+    const parameters = PluginManager.parameters(pluginName);
+    const myParameter = Boolean(parameters["My Parameter"]) || true;
+    console.log(myParameter);
+})();
